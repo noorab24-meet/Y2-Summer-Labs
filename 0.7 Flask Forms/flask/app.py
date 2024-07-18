10656a692bae6d541629d8b4dd21edb1a035bc0e
noorab24-meet <noorabdalla232@gmail.com>
2024-07-18 10:30:29 +0300

    111

--- 0.7 Flask Forms/flask/app.py
+++ 0.7 Flask Forms/flask/app.py
@@ -5,7 +5,7 @@ app = Flask(__name__,
 template_folder='templates',
 static_folder='static')
 fortunes=[
-    "Dont be panick bbecause of CS", #Supposed to be comma in list!!
+    "Dont be panick bbecause of CS", #Supposed to be comma in list
     "Life is nice",
     "Dont let CS ruin your life",
     "If you have an error:",
@@ -19,21 +19,10 @@ fortunes=[
 
 @app.route('/home', methods=['GET', 'POST'])
 def home():
-    # if request.method == 'POST':
-    #     birth_month = request.form['birth_month']
-    #     return redirect(url_for('fortune', birth_month=birth_month))
-    # else:
-    #     return render_template('hello.html')
-
-     # Check if birth_month is passed as a query parameter
-    if request.method == 'GET':
-        return render_template('hello.html')
-    else:
+    if request.method == 'POST':
         birth_month = request.form['birth_month']
-
-        #default text
-        fortune_text = "no fortune default"
-
+        
+        #checking for user input 
         if birth_month:
             index = len(birth_month)
             chosen_fortune = fortunes[index]
@@ -42,42 +31,24 @@ def home():
             # This for if birth_month is not provided, it will choose a random fortune from the list
             chosen_fortune = random.choice(fortunes)
             fortune_text = f"Your random fortune is: {chosen_fortune}"
+   
+        if index > 10:  
+            return render_template('fortune.html', fortune = "Error", birth_month=birth_month)  
+        else: 
+            return render_template('fortune.html', fortune=fortune_text, birth_month=birth_month)
+    else:
+        return render_template('hello.html')
 
-        return render_template('fortune.html', fortune=fortune_text)   
+    
 
-
-# @app.route('/fortune')
-# def fortune():
-#     # Check if birth_month is passed as a query parameter
-#     birth_month = request.form['birth_month']
-
-#     #default text
-#     fortune_text = "no fortune default"
-
-#     if birth_month:
-#         index = len(birth_month)
-#         chosen_fortune = fortunes[index]
-#         fortune_text = f"Your fortune is: {chosen_fortune}"
-#     else:
-#         # This for if birth_month is not provided, it will choose a random fortune from the list
-#         chosen_fortune = random.choice(fortunes)
-#         fortune_text = f"Your random fortune is: {chosen_fortune}"
-
-#     return render_template('fortune.html', fortune=fortune_text)
-
-
-# def fortune():
-#     birth_month = request.args.get('birth_month')
-#     if birth_month:
-#         random.seed(birth_month)  #  I uesed it for seed random with birth_month
-#         chosen_fortune = random.choice(fortunes)
-#         fortune_text = "Your fortune for " + birth_month + " is: " + chosen_fortune
-#     else:
-#         chosen_fortune = random.choice(fortunes) #  I puted for choosing a random fortune
-#         fortune_text = "Your random fortune is: " + chosen_fortune
-
-#     return render_template('fortune.html', fortune=fortune_text)
-
+    
+  
+    # number_of_birth_month = len(birth_month)
+    # number_of_the_list = len(fortunes)
+    # if number_of_birth_month > number_of_the_list:
+    #     return render_template('fortune.html', fortune = "error")  
+    # else: 
+    #     return render_template('fortune.html', fortune=fortune_text)  
 
 if __name__ == '__main__':
     app.run(debug=True)
