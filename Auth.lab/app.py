1a4ce0cc28da7019978c11bae97098447ebace8f
noorab24-meet <noorabdalla232@gmail.com>
2024-07-22 22:25:59 +0300

    have fun!

--- Auth.lab/app.py
+++ Auth.lab/app.py
@@ -4,33 +4,47 @@ import pyrebase
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'super-secret-key'
 
-Config = {
-    "apiKey": "AIzaSyDSlzpcauMpjdvXBRo3IOWu4Yi6S_GwqNU",
-    "authDomain": "auth-lab-bfd14.firebaseapp.com",
-    "projectId": "auth-lab-bfd14",
-    "storageBucket": "auth-lab-bfd14.appspot.com",
-    "messagingSenderId": "53978111196",
-    "appId": "1:53978111196:web:33254ef92b816de25f6819",
-    "measurementId": "G-76KPB174YX",
-    "databaseURL": ""
+Config= {
+  "apiKey": "AIzaSyDSlzpcauMpjdvXBRo3IOWu4Yi6S_GwqNU",
+  "authDomain": "auth-lab-bfd14.firebaseapp.com",
+  "databaseURL": "https://auth-lab-bfd14-default-rtdb.europe-west1.firebasedatabase.app",
+  "projectId": "auth-lab-bfd14",
+  "storageBucket": "auth-lab-bfd14.appspot.com",
+  "messagingSenderId": "53978111196",
+  "appId": "1:53978111196:web:33254ef92b816de25f6819",
+  "measurementId": "G-76KPB174YX"
 }
+
 
 firebase = pyrebase.initialize_app(Config)
 auth = firebase.auth()
+db =firebase.database()
 
 @app.route('/', methods=['GET', 'POST'])
 def signup():
     if request.method == 'POST':
+        full_name = request.form['full_name']
+        username = request.form['username']
         email = request.form['email']
         password = request.form['password']
         try:
             user = auth.create_user_with_email_and_password(email, password)
             session['user'] = user['idToken']
+            session['user_id'] = user['localId']
+            session['user_name'] = full_name
+            user_data = {
+                "full_name": full_name,
+                "email": email,
+                "username": username
+            }
+            db.child("Users").child(user['localId']).set(user_data)
             session['quotes'] = []
             return redirect(url_for('home'))
         except Exception as e:
-            return f"Error creating account: {e}"
+            error_message = str(e)
+            return render_template('error.html', error_message=error_message)
     return render_template('signup.html')
+
 
 @app.route('/signin', methods=['GET', 'POST'])
 def signin():
@@ -40,11 +54,14 @@ def signin():
         try:
             user = auth.sign_in_with_email_and_password(email, password)
             session['user'] = user['idToken']
+            session['user_id'] = user['localId']
             session['quotes'] = []
             return redirect(url_for('home'))
         except Exception as e:
-            return f"Error signing in: {e}"
+            error_message = str(e)
+            return render_template('error.html', error_message=error_message)
     return render_template('signin.html')
+
 
 @app.route('/signout', methods=['POST'])
 def signout():
@@ -57,12 +74,20 @@ def home():
     if 'user' not in session:
         return redirect(url_for('signin'))
     if request.method == 'POST':
-        quote = request.form['quote']
-        session['quotes'].append(quote)
+        quote_text = request.form['quote']
         session.modified = True
-
+        said_by = request.form['speaker']
+        additional_info = request.form.get('additional_info', '')
+        quote_dict = {
+            "quote": quote_text,
+            "speaker": said_by,
+            "additional_info": additional_info,
+            "uid": session['user']
+        }
+        db.child("Quotes").push(quote_dict)
         return redirect(url_for('thanks'))
-    return render_template('home.html')
+    return render_template('home.html')    
+
 
 @app.route('/thanks')
 def thanks():
@@ -72,7 +97,13 @@ def thanks():
 def display():
     if 'user' not in session:
         return redirect(url_for('signin'))
-    return render_template('display.html', quotes=session['quotes'])
+    quotes = db.child("Quotes").get().val()
+    if quotes is None:
+        quotes = []
+    else:
+        quotes = list(quotes.values())
+    return render_template('display.html', quotes=quotes)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
